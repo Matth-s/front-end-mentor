f0c8129aecb390b83c99072962ab80a99dca82b9
Matth-s <senosiain.matthieu@yahoo.fr>
2023-03-15 08:55:48 +0100

    build test

--- src/components/Category.jsx
+++
@@ -1,9 +0,0 @@
-const Category = ({ category }) => {
-  return (
-    <div className="category-container border-radius flex flex__alignCenter">
-      <p className="body4">{category}</p>
-    </div>
-  );
-};
-
-export default Category;
