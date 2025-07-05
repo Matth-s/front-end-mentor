daa36a56203b8ab34cbab6e4351c5baf1c43163c
Matthieu senosiain <senosiain.matthieu@yahoo.fr>
2025-07-05 11:00:14 +0200

    last

--- src/firebase-conf.js
+++ src/firebase-conf.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD2VeJ8iETxJJ5TT2GWB4ugOptuR0rmuac',
-  authDomain: 'front-end-mentor-22155.firebaseapp.com',
-  projectId: 'front-end-mentor-22155',
-  storageBucket: 'front-end-mentor-22155.firebasestorage.app',
-  messagingSenderId: '581262020634',
-  appId: '1:581262020634:web:3b5d7ba783ab21073981a3',
-  measurementId: 'G-9YX1Z4GVRL',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
